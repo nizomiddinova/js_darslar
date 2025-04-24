7b7ff5ffc54214b22a4e0a655d0ecab71091ef4c
nizomiddinova <nizomiddinovadilafruz1007@gmail.com>
2025-04-24 18:15:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// O'zgaruvchi qiymat 
+let name = "Dilafruz";
+// console.log(name);
+
+name = "Diladora"
+console.log(typeof name);     //turini aniqlab beradi
+
+
+// O'zgarmas qiymat
+const age = 18;
+console.log(typeof age);
+
+
+// Eski usul 
+var city = "Toshkent";
+console.log(typeof city);
+
+
